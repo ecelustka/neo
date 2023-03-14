d76ecdbaed77f1de040dbdcc2571a1e6fba12b90
Emil Čelustka <ecelustka@EmilPro14.local>
2023-03-14 19:06:49 +0100

    fix: formatters

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -27,7 +27,31 @@ require("formatter").setup({
 			require("formatter.filetypes.json").prettier,
 		},
 		lua = {
-			require("formatter.filetypes.lua").luaformat,
+			-- "formatter.filetypes.lua" defines default configurations for the
+			-- "lua" filetype
+			require("formatter.filetypes.lua").stylua,
+
+			-- You can also define your own configuration
+			function()
+				-- Supports conditional formatting
+				if util.get_current_buffer_file_name() == "special.lua" then
+					return nil
+				end
+
+				-- Full specification of configurations is down below and in Vim help
+				-- files
+				return {
+					exe = "stylua",
+					args = {
+						"--search-parent-directories",
+						"--stdin-filepath",
+						util.escape_path(util.get_current_buffer_file_path()),
+						"--",
+						"-",
+					},
+					stdin = true,
+				}
+			end,
 		},
 		markdown = {
 			require("formatter.filetypes.markdown").prettier,
