ae6c17ce1996cee4392988e732bc176e4f7dd197
Emil Čelustka <ecelustka@emilpro14.local>
2024-04-27 16:34:46 +0200

    refactor: cmp

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -1,14 +1,22 @@
 local cmp = require("cmp")
 
 cmp.setup({
+	enabled = true,
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "path" },
+		{ name = "buffer" },
+		{ name = "nvim_lua" },
+		{ name = "luasnip" },
+	},
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
 	min_length = 8,
 	window = {
 		completion = cmp.config.window.bordered(),
 		documentation = cmp.config.window.bordered(),
 	},
-	experimental = {
-		native_menu = false,
-		ghost_text = true,
-	},
-	completion = { completeopt = "menu,menuone,noinsert" },
 })
