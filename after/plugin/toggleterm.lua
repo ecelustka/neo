19214c5e0538ce36a86554437ab630083aab3c16
Emil Čelustka <ecelustka@emilpro14.local>
2024-04-27 16:34:40 +0200

    add new terminal to toggle term

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,7 +1,7 @@
 require("toggleterm").setup({})
 
 vim.keymap.set("n", "<C-t>", ":ToggleTerm<CR>")
-vim.keymap.set("n", "<C-T>", ":ToggleTerm 2<CR>")
+vim.keymap.set("n", "<C-t>2", ":ToggleTerm 2<CR>")
 
 function _G.set_terminal_keymaps()
 	local opts = { buffer = 0 }
